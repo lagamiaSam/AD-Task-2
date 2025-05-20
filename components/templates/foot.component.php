754f438cfae4615bc8f6ef95d74b1263282631db
lagamiaSam <167587359+lagamiaSam@users.noreply.github.com>
2025-05-20 14:38:01 +0800

    feat(landing page): add foot component

--- components/templates/foot.component.php
+++ components/templates/foot.component.php
@@ -2,9 +2,10 @@
 function renderFooter() {
     ?>
     <footer>
-        <p>&copy; <?= date('Y') ?> SneakHub Project. All rights reserved.</p>
+        <p>&copy; <?= date('Y'); ?> SneakHub Project. All rights reserved.</p>
     </footer>
     </body>
     </html>
-    <?php
+<?php
 }
+?>
