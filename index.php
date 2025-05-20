d4624483e76991db46ee7b232bd50d6167005ccb
lagamiaSam <167587359+lagamiaSam@users.noreply.github.com>
2025-05-20 14:38:51 +0800

    feat(landing page): add BASE PATH

--- index.php
+++ index.php
@@ -1,23 +1,14 @@
 <?php
 require BASE_PATH . '/vendor/autoload.php';
-require BASE_PATH . '/utils/htmlEscape.utils.php';
 
-require_once BASE_PATH . '/handlers/pageData.handler.php';
-require_once BASE_PATH . '/components/cards/note.component.php';
 require_once BASE_PATH . '/components/templates/foot.component.php';
 require_once BASE_PATH . '/components/templates/head.component.php';
 require_once BASE_PATH . '/components/templates/nav.component.php';
+require_once BASE_PATH . '/components/customs/hero.components.php';
 ?>
+
 
 <?php
-head('Product List');
-navHeader($navList);
+renderFooter();
 ?>
 
-<section class="grid grid-cols-3">
-    <?php displayCardForm($productList); ?>
-</section>
-
-<?php
-footer();
-?>
